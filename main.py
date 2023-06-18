22645f40bd921ad591fdcc1612f44e99c8df4192
Ricardo Avelar <50161316+ricardodiasmode@users.noreply.github.com>
2023-06-17 22:59:35 -0300

    almost done

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ class AdaBoost:
         weights = np.ones(n_samples) / n_samples  # Inicializando os pesos igualmente
 
         for _ in range(self.n_estimators):
-            estimator = DecisionTreeClassifier(max_depth=1)  # Base Estimator: Decision Tree de profundidade 1
+            error = 0
+            estimator = DecisionTreeClassifier(max_depth=3)  # Base Estimator: Decision Tree de profundidade 1
             estimator.fit(x, y, sample_weight=weights)  # Treinando o estimador com os pesos
 
             y_pred = estimator.predict(x)
 
-            error =  # Calculando o erro ponderado
+            # Calculando o erro
+            for i in range(len(y_pred)):
+                if y_pred[i] != y[i]:
+                    error = weights[i] + error
+
             if error >= 0.5:
                 break  # Se o erro for maior ou igual a 0.5, interrompe o loop
 
@@ -26,7 +31,12 @@ class AdaBoost:
 
             estimator_weight = 0.5 * np.log((1 / error) - 1)  # Calculando o peso do estimador
 
-            weights *= np.exp(estimator_weight * misclassified)  # Atualizando os pesos corretamente
+            for i in range(len(y_pred)):
+                if y_pred[i] != y[i]:
+                    weights[i] = weights[i] * np.exp(estimator_weight)
+                else:
+                    weights[i] = weights[i] * np.exp(-estimator_weight)
+
             weights /= np.sum(weights)  # Normalizando os pesos
 
             self.estimators.append(estimator)
@@ -45,7 +55,7 @@ class AdaBoost:
 
 
 def get_data():
-    x, y = np.empty([958, 9], dtype=list), np.empty([958, 1], dtype=int)
+    x, y = np.empty([958, 9], dtype=list), np.empty([958], dtype=int)
     # open file
     with open('tic+tac+toe+endgame/tic-tac-toe.data', 'r') as data_file:
         # read all lines
@@ -68,12 +78,16 @@ def get_data():
             if line_splitted[-1].strip() == 'positive':
                 y[i] = 1
             else:
-                y[i] = 0
+                y[i] = -1
 
         return x, y
 
 
 features, labels = get_data()
+
+combined_list = list(zip(features, labels))
+np.random.shuffle(combined_list)
+features[:], labels[:] = zip(*combined_list)
 
 boosting = AdaBoost(n_estimators=3)
 
@@ -82,4 +96,11 @@ boosting.fit(features_train, labels[:95])
 
 features_test = features[95:]
 labels_pred = boosting.predict(features_test)
-print("Previsões:", labels_pred)
+
+# TODO: Cross Validation
+# Calculando a acurácia
+accuracy = 0
+for i in range(len(labels_pred)):
+    if labels_pred[i] == labels[i + 95]:
+        accuracy += 1
+print('Acurácia: ', accuracy / len(labels_pred))
