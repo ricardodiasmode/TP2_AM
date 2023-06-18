49982831cf23782bf1d7b1c7c6b35cb9824238b3
Ricardo Avelar <50161316+ricardodiasmode@users.noreply.github.com>
2023-06-17 23:55:34 -0300

    done

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+
+
+def get_estimator(in_estimator_type, in_tree_depth):
+    if in_estimator_type == 'tree':
+        return DecisionTreeClassifier(max_depth=in_tree_depth)
+    elif in_estimator_type == 'svm':
+        return SVC(kernel='linear', C=0.8)
+
 
 class AdaBoost:
     def __init__(self, n_estimators):
@@ -7,13 +17,14 @@ class AdaBoost:
         self.estimators = []
         self.estimator_weights = []
 
-    def fit(self, x, y):
+    def fit(self, x, y, in_tree_depth, estimator_type):
         n_samples = x.shape[0]
         weights = np.ones(n_samples) / n_samples  # Inicializando os pesos igualmente
 
         for _ in range(self.n_estimators):
             error = 0
-            estimator = DecisionTreeClassifier(max_depth=3)  # Base Estimator: Decision Tree de profundidade 1
+            estimator = get_estimator(estimator_type, in_tree_depth)
+
             estimator.fit(x, y, sample_weight=weights)  # Treinando o estimador com os pesos
 
             y_pred = estimator.predict(x)
@@ -24,6 +35,7 @@ class AdaBoost:
                     error = weights[i] + error
 
             if error >= 0.5:
+                print('Erro maior ou igual a 0.5')
                 break  # Se o erro for maior ou igual a 0.5, interrompe o loop
 
             if error == 0:
@@ -50,8 +62,6 @@ class AdaBoost:
             y_pred += estimator_weight * estimator.predict(x_predict)
 
         return np.sign(y_pred)
-
-
 
 
 def get_data():
@@ -83,24 +93,57 @@ def get_data():
         return x, y
 
 
+def k_fold_cross_validation(in_features, in_labels, fold_amount, in_estimators, in_tree_depth, in_estimator_type):
+    fold_size = len(in_features) // fold_amount
+    accuracies = []
+
+    for i in range(fold_amount):
+        start = i * fold_size
+        end = (i + 1) * fold_size
+
+        features_train = np.concatenate((in_features[:start], in_features[end:]), axis=0)
+        labels_train = np.concatenate((in_labels[:start], in_labels[end:]), axis=0)
+        features_test = in_features[start:end]
+        labels_test = in_labels[start:end]
+
+        boosting = AdaBoost(n_estimators=in_estimators)
+        boosting.fit(features_train, labels_train, in_tree_depth, in_estimator_type)
+        labels_pred = boosting.predict(features_test)
+
+        in_accuracy = np.sum(labels_pred == labels_test) / len(labels_test)
+        accuracies.append(in_accuracy)
+
+    return np.mean(accuracies)
+
+
 features, labels = get_data()
 
 combined_list = list(zip(features, labels))
 np.random.shuffle(combined_list)
 features[:], labels[:] = zip(*combined_list)
 
-boosting = AdaBoost(n_estimators=3)
+n_folds = 5
 
-features_train = features[:95]
-boosting.fit(features_train, labels[:95])
+# Testando a acurácia em função do número de estimadores e do tipo
+accuracy = []
+for estimator_type in ['tree', 'svm']:
+    for estimators in range(3, 10):
+        tree_depth = 3
+        accuracy.append(k_fold_cross_validation(features, labels, n_folds, estimators, tree_depth, estimator_type))
+        plt.plot(estimators, accuracy[estimators - 3], 'bo')
+    plt.xlabel('Número de estimadores')
+    plt.ylabel('Acurácia')
+    plt.title('Acurácia x Número de estimadores' + ' (' + estimator_type + ')')
+    plt.axis([2, 10, 0.5, 1])
+    plt.show()
 
-features_test = features[95:]
-labels_pred = boosting.predict(features_test)
-
-# TODO: Cross Validation
-# Calculando a acurácia
-accuracy = 0
-for i in range(len(labels_pred)):
-    if labels_pred[i] == labels[i + 95]:
-        accuracy += 1
-print('Acurácia: ', accuracy / len(labels_pred))
+# Testando a acurácia em função da profundidade da árvore
+for tree_depth in range(1, 25):
+    estimators = 3
+    accuracy.append(k_fold_cross_validation(features, labels, n_folds, estimators, tree_depth, 'tree'))
+    plt.plot(tree_depth, accuracy[tree_depth - 1], 'bo')
+plt.xlabel('Profundidade da árvore')
+plt.ylabel('Acurácia')
+plt.title('Acurácia x Profundidade da árvore')
+plt.axis([0, 25, 0.5, 1])
+plt.show()
